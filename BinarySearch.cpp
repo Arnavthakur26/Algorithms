5ca22d03eaf3ecdc6af9c819ed0ca88ecfc75894
Arnav Thakur <arnavthakur206@gmail.com>
2022-09-26 09:04:21 +0530

    initial commit

---
+++ BinarySearch.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int size, int key)
+{
+    int start=arr[0];
+    int end=size-1;
+
+    // int mid= (start+end)/2;
+
+    int mid= start + (end-start)/2; //for edge cases -> if start + end > INT_MIN
+
+    while(start <= end){
+        if(arr[mid]==key){
+            return mid;
+        }
+        else if(arr[mid]>key){
+            end=mid-1;
+        }
+        else if (arr[mid<key]){
+            start=mid+1;
+        }
+        mid=(start+end)/2;
+                    
+    }
+    return -1;
+
+}
+
+int main()
+{
+    int arr[]={2,4,6,8,12,18};
+
+    int index=binarySearch(arr,6,12);
+
+    cout<<"Key 12 is at index "<<index<<endl;
+}
